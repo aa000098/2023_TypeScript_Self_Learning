4ab58cc97d11cff164c89e8041c3c0ffec2131b1
HyunSon <aa000098@naver.com>
2023-11-06 13:46:01 +0900

    feat: lecture

---
+++ 1_basic/2_basics.js
@@ -0,0 +1,17 @@
+"use strict";
+/**
+ * Types
+ */
+let helloText = 'Hello';
+// helloText = true;
+/**
+ * 7개의 타입
+ */
+const stringVar = 'String';
+const numberVar = 3;
+const bigIntVar = BigInt(999999999);
+// tsconfig.json target 2016에서 2020 버전으로 바꿔주기
+const booleanVar = true;
+const symbolVar = Symbol(1);
+const nullVar = null;
+const undefinedVar = undefined;
---
+++ 1_basic/4_enum.ts
@@ -0,0 +1,77 @@
+/**
+ * Enum
+ */
+
+/**
+ * API 요청
+ * 상태는 4가지 상태
+ * 
+ * DONE - 요청 완료 상태
+ * ERROR - 에러가 생긴 상태
+ * LOADING - 로딩 상태
+ * INITIAL - 초기 상태
+ */
+function runWork() {
+    let state = 'INITIAL';
+
+    try {
+        state = 'LOADING';
+        //작업을 한다.'
+
+        state = 'DONE';
+    } catch (e) {
+        state = "ERROR";
+    } finally {
+        return state;
+    }
+}
+
+console.log(runWork() === 'DONE');
+
+const doneState = 'DONE';
+const loadingState = 'LOADING';
+const errorState = 'ERROR';
+const initialState = 'INITIAL';
+
+function runWork2() {
+    let state = initialState;
+
+    try {
+        state = loadingState;
+        //작업을 한다.'
+
+        state = doneState;
+    } catch (e) {
+        state = errorState;
+    } finally {
+        return state;
+    }
+}
+
+console.log(runWork2() === doneState);
+
+enum State {
+    DONE,
+    LOADING,
+    INITIAL,
+    ERROR,
+}
+
+function runWork3() {
+    let state = State.INITIAL;
+
+    try {
+        state = State.LOADING;
+        //작업을 한다.'
+
+        state = State.DONE;
+    } catch (e) {
+        state = State.ERROR;
+    } finally {
+        return state;
+    }
+}
+
+console.log(runWork3() === State.DONE);
+console.log(State.DONE);
+
